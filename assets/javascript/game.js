8084ce8c3ac57615ef09bc70b8ed7b6b43c7007b
chrisjal <cjalallian@gmail.com>
2017-07-30 01:38:12 -0700

    test changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,49 @@
 // Revisions:
 // Wins, losses, create variable for player has chosen and waiting for opponent. if
 //both chosen then display results and play animation/so and so wins.
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAXyx-DEaqecadD4OV6576voVgNFjyO-us",
+	authDomain: "rock-paper-scissors-357f2.firebaseapp.com",
+	databaseURL: "https://rock-paper-scissors-357f2.firebaseio.com",
+	projectId: "rock-paper-scissors-357f2",
+	// storageBucket: "rock-paper-scissors-357f2.appspot.com",
+	// messagingSenderId: "863882614549"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var playerName = "";
+var playerChoice = "";
+var playerLosses = 0;
+var playerWins = "";
+var playerChat = "";
+
+$("#submit-input").on("click", function() {
+	event.preventDefault();
+	playerName = $("#name-input").val().trim();
+
+
+	database.ref("players/").push( {
+		player: playerName
+	});
+	$("img").on("click", function() {
+ 	// console.log(this);
+ 	var playerChoice = $(this).attr("data-value");
+
+	database.ref("players/").push({
+ 		choice: playerChoice
+	});
+});
+
+});
+
+// database.ref().on("child_added", function(snapshot) {
+// 	var snapshotValue = snapshot.val();
+// })
+
+
+
+
