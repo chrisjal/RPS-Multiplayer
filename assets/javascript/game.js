41180b23d5a25ccaeaec6612717c9b265db5674c
chrisjal <cjalallian@gmail.com>
2017-07-29 21:47:29 -0700

    basic HTML/starting js outline

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// Created July 29, 2017 17:00
+// Author: Chris Jalallian
+// Revisions:
+// Wins, losses, create variable for player has chosen and waiting for opponent. if
+//both chosen then display results and play animation/so and so wins.
